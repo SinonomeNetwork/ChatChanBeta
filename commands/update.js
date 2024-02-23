5217c39c9f6849a56e7875f5ad93694338b7fa89
ringoXD <nep360i@gmail.com>
2024-02-24 00:35:45 +0900

    ちゃんとした

--- commands/update.js
+++ commands/update.js
@@ -17,18 +17,18 @@ module.exports = {
     	return;
     }
 
-	let msg = ">".yellow + " npm i\n".green;
+	let msg = ">".yellow + " git pull\n".green;
     await interaction.reply("更新中...\n" + "```ansi\n" + msg + "\n```");
     let lock = false;
     let lockTimeout = null;
-    let npmprocess = childprocess.spawn("npm", ["i"], {
+    let gitProcess = childprocess.spawn("git", ["-c", "color.ui=always", "pull"], {
         cwd: path.resolve(__dirname)
     });
     let npmtimeout = setTimeout(() => {
-        npmprocess.kill();
+        gitProcess.kill();
         interaction.editReply(`3分経っても処理が完了しなかったため、強制終了しました`);
     }, 1000 * 60 * 3);
-    npmprocess.stdout.on("data", data => {
+    gitProcess.stdout.on("data", data => {
         msg += data.toString().replace(/\x1b\[m/g, "\x1b[0m");
         if (!lock) {
             interaction.editReply("更新中...\n" + "```ansi\n" + msg + "\n```");
@@ -40,22 +40,22 @@ module.exports = {
             lock = false;
         }, 1000);
     });
-    npmprocess.stderr.on("data", data => {
+    gitProcess.stderr.on("data", data => {
         console.error(data.toString())
     })
-    npmprocess.on("close", async () => {
+    gitProcess.on("close", async () => {
         clearTimeout(npmtimeout);
         await interaction.editReply("```ansi\n" + msg + "\n```\n");
 
-		let msg2 = ">".yellow + " git pull\n".green;
-		let gitProcess = childprocess.spawn("git", ["-c", "color.ui=always", "pull"], {
+		let msg2 = ">".yellow + " npm i\n".green;
+		let npmProcess = childprocess.spawn("npm", ["i"], {
  	    	cwd: path.resolve(__dirname)
  	   	});
  	  	let gittimeout = setTimeout(() => {
- 	    	gitProcess.kill();
+ 	    	npmProcess.kill();
  	    	interaction.editReply(`3分経っても処理が完了しなかったため、強制終了しました`);
  		}, 1000 * 60 * 3);
- 	   	gitProcess.stdout.on("data", data => {
+ 	   	npmProcess.stdout.on("data", data => {
  	    	msg2 += data.toString().replace(/\x1b\[m/g, "\x1b[0m");
  	    	if (!lock) {
  	           interaction.editReply("更新中...\n" + "```ansi\n" + msg + "\n```\n" + "```ansi\n" + msg2 + "\n```");
@@ -67,10 +67,10 @@ module.exports = {
  	           lock = false;
  	       	}, 1000);
  	   });
- 	   	gitProcess.stderr.on("data", data => {
+ 	   	npmProcess.stderr.on("data", data => {
  	       console.error(data.toString())
  	  	})
- 	  	gitProcess.on("close", async () => {
+ 	  	npmProcess.on("close", async () => {
  	       clearTimeout(gittimeout);
  	       await interaction.editReply("```ansi\n" + msg + "\n```\n" + "```ansi\n" + msg2 + "\n```\n" + "完了\nBOTを再起動します");
  	       setTimeout(() => {
